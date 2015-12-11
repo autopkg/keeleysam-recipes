aea7f58af43c9e1ec4b419f88ceceba48334aedc
Samuel Keeley <samuel.keeley@airbnb.com>
2015-12-11 10:38:28 -0800

    pep8

--- ParallelsDesktop/ParallelsURLProvider.py
+++ ParallelsDesktop/ParallelsURLProvider.py
@@ -26,19 +26,19 @@ from autopkglib import Processor, ProcessorError
 __all__ = ["ParallelsURLProvider"]
 
 URLS = {
-	"ParallelsDesktop6": "http://update.parallels.com/desktop/v6/en_us/parallels/parallels_updates.xml",
-	"ParallelsDesktop7": "http://update.parallels.com/desktop/v7/parallels/parallels_updates.xml",
+    "ParallelsDesktop6": "http://update.parallels.com/desktop/v6/en_us/parallels/parallels_updates.xml",
+    "ParallelsDesktop7": "http://update.parallels.com/desktop/v7/parallels/parallels_updates.xml",
     "ParallelsDesktop8": "http://update.parallels.com/desktop/v8/parallels/parallels_updates.xml",
     "ParallelsDesktop9": "http://update.parallels.com/desktop/v9/parallels/parallels_updates.xml",
-    "ParallelsDesktop10": "http://update.parallels.com/desktop/v10/parallels/parallels_updates.xml"
-        }
+    "ParallelsDesktop10": "http://update.parallels.com/desktop/v10/parallels/parallels_updates.xml"}
+
 
 class ParallelsURLProvider(Processor):
     description = "Provides a version, description, and DMG download for the Parallels product given."
     input_variables = {
         "product_name": {
-        "required": True,
-        "description": "Product to fetch URL for. One of 'ParallelsDesktop6', 'ParallelsDesktop7', 'ParallelsDesktop8', 'ParallelsDesktop9', 'ParallelsDesktop10'.",
+            "required": True,
+            "description": "Product to fetch URL for. One of 'ParallelsDesktop6', 'ParallelsDesktop7', 'ParallelsDesktop8', 'ParallelsDesktop9', 'ParallelsDesktop10'.",
         },
     }
     output_variables = {
@@ -56,44 +56,57 @@ class ParallelsURLProvider(Processor):
 
     def main(self):
 
-		def compare_version(a, b):
-			return cmp(LooseVersion(a), LooseVersion(b))
+        def compare_version(a, b):
+            return cmp(LooseVersion(a), LooseVersion(b))
 
-		valid_prods = URLS.keys()
-		prod = self.env.get("product_name")
-		if prod not in valid_prods:
-			raise ProcessorError("product_name %s is invalid; it must be one of: %s" % (
-								prod, valid_prods))
-		url = URLS[prod]
-		try:
-			manifest_str = urllib2.urlopen(url).read()
-		except BaseException as e:
-			raise ProcessorError("Unexpected error retrieving product manifest: '%s'" % e)
+        valid_prods = URLS.keys()
+        prod = self.env.get("product_name")
+        if prod not in valid_prods:
+            raise ProcessorError(
+                "product_name %s is invalid; it must be one of: %s" %
+                (prod, valid_prods))
+        url = URLS[prod]
+        try:
+            manifest_str = urllib2.urlopen(url).read()
+        except BaseException as e:
+            raise ProcessorError(
+                "Unexpected error retrieving product manifest: '%s'" %
+                e)
 
-		the_xml = xml.dom.minidom.parseString(manifest_str)
-		products = the_xml.getElementsByTagName('Product')
-		parallels = None
-		for a_product in products:
-			# Find the products that are 'Parallels Desktop'
-			if a_product.getElementsByTagName('ProductName')[0].firstChild.nodeValue == u'Parallels Desktop':
-				parallels = a_product
-				v_major         = parallels.getElementsByTagName('Major')[0].firstChild.nodeValue
-				v_minor         = parallels.getElementsByTagName('Minor')[0].firstChild.nodeValue
-				v_sub_minor     = parallels.getElementsByTagName('SubMinor')[0].firstChild.nodeValue
-				v_sub_sub_minor = parallels.getElementsByTagName('SubSubMinor')[0].firstChild.nodeValue
-				version = '.'.join([v_major, v_minor, v_sub_minor, v_sub_sub_minor])
-				update = parallels.getElementsByTagName('Update')[0]
-				try:
-					description = [x.firstChild.nodeValue for x in update.getElementsByTagName('UpdateDescription') if x.firstChild.nodeValue.startswith('en_US')][0]
-					description = '<html><body>%s</body></html>' % (description.split('#',1)[-1])
-				except:
-					description = [x.firstChild.nodeValue for x in update.getElementsByTagName('UpdateDescription')][0]
-				url = update.getElementsByTagName('FilePath')[0].firstChild.nodeValue
+        the_xml = xml.dom.minidom.parseString(manifest_str)
+        products = the_xml.getElementsByTagName('Product')
+        parallels = None
+        for a_product in products:
+            # Find the products that are 'Parallels Desktop'
+            if a_product.getElementsByTagName(
+                    'ProductName')[0].firstChild.nodeValue == u'Parallels Desktop':
+                parallels = a_product
+                v_major = parallels.getElementsByTagName(
+                    'Major')[0].firstChild.nodeValue
+                v_minor = parallels.getElementsByTagName(
+                    'Minor')[0].firstChild.nodeValue
+                v_sub_minor = parallels.getElementsByTagName(
+                    'SubMinor')[0].firstChild.nodeValue
+                v_sub_sub_minor = parallels.getElementsByTagName(
+                    'SubSubMinor')[0].firstChild.nodeValue
+                version = '.'.join(
+                    [v_major, v_minor, v_sub_minor, v_sub_sub_minor])
+                update = parallels.getElementsByTagName('Update')[0]
+                try:
+                    description = [x.firstChild.nodeValue for x in update.getElementsByTagName(
+                        'UpdateDescription') if x.firstChild.nodeValue.startswith('en_US')][0]
+                    description = '<html><body>%s</body></html>' % (
+                        description.split('#', 1)[-1])
+                except:
+                    description = [x.firstChild.nodeValue for x in update.getElementsByTagName(
+                        'UpdateDescription')][0]
+                url = update.getElementsByTagName(
+                    'FilePath')[0].firstChild.nodeValue
 
-		self.env["version"] = version
-		self.env["description"] = description
-		self.env["url"] = url
-		self.output("Found URL %s" % self.env["url"])
+        self.env["version"] = version
+        self.env["description"] = description
+        self.env["url"] = url
+        self.output("Found URL %s" % self.env["url"])
 
 if __name__ == "__main__":
     processor = ParallelsURLProvider()
