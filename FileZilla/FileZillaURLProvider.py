94d81d1e796db36cd4f56fab8de5ceca6a04f966
Samuel Keeley <sam.keeley@gmail.com>
2015-12-11 10:38:28 -0800

    pep8

--- FileZilla/FileZillaURLProvider.py
+++ FileZilla/FileZillaURLProvider.py
@@ -25,10 +25,11 @@ __all__ = ["FileZillaURLProvider"]
 update_url = "http://update.filezilla-project.org/updatecheck.php?platform=i686-apple-darwin9&version=3.0.0&osversion=12"
 
 prods = {"FileZilla": "release",
-		"FileZilla_release": "release",
-        "FileZilla_beta": "beta",
-        "FileZilla_nightly": "nightly"
-        }
+         "FileZilla_release": "release",
+         "FileZilla_beta": "beta",
+         "FileZilla_nightly": "nightly"
+         }
+
 
 class FileZillaURLProvider(Processor):
     description = "Provides a version and compressed download for the FileZilla product given."
@@ -50,50 +51,53 @@ class FileZillaURLProvider(Processor):
     __doc__ = description
 
     def main(self):
-		
-		valid_prods = prods.keys()
-		prod = self.env.get("product_name")
-		if prod not in valid_prods:
-			raise ProcessorError("product_name %s is invalid; it must be one of: %s" % (
-								prod, valid_prods))
-	
-		try:
-			manifest_str = urllib2.urlopen(update_url).read()
-			#print manifest_str
-		except BaseException as e:
-			raise ProcessorError("Unexpected error retrieving product manifest: '%s'" % e)
-	
-		version = None
-		url = None
 
-		if prod == "FileZilla_beta":
-			for item in manifest_str.split("\n"):
- 				if "beta" in item:
-					item_strip = item.strip()
- 					line_split = item_strip.split()
- 					url = line_split[2]
- 					version = line_split[1]
- 		elif prod == "FileZilla_nightly":
- 			for item in manifest_str.split("\n"):
- 				if "nightly" in item:
-					item_strip = item.strip()
- 					line_split = item_strip.split()
- 					url = line_split[2]
- 					version = line_split[1]
- 		elif prod == "FileZilla" or prod == "FileZilla_release":
- 			for item in manifest_str.split("\n"):
- 				if "release" in item:
-					item_strip = item.strip()
- 					line_split = item_strip.split()
- 					url = line_split[2]
- 					version = line_split[1]
+        valid_prods = prods.keys()
+        prod = self.env.get("product_name")
+        if prod not in valid_prods:
+            raise ProcessorError(
+                "product_name %s is invalid; it must be one of: %s" %
+                (prod, valid_prods))
 
-		if not version or not url:
-			raise ProcessorError("Version or URL not found")
-		
-		self.env["version"] = version
-		self.env["url"] = url
-		self.output("Found URL %s" % self.env["url"])
+        try:
+            manifest_str = urllib2.urlopen(update_url).read()
+            # print manifest_str
+        except BaseException as e:
+            raise ProcessorError(
+                "Unexpected error retrieving product manifest: '%s'" %
+                e)
+
+        version = None
+        url = None
+
+        if prod == "FileZilla_beta":
+            for item in manifest_str.split("\n"):
+                if "beta" in item:
+                    item_strip = item.strip()
+                    line_split = item_strip.split()
+                    url = line_split[2]
+                    version = line_split[1]
+        elif prod == "FileZilla_nightly":
+            for item in manifest_str.split("\n"):
+                if "nightly" in item:
+                    item_strip = item.strip()
+                    line_split = item_strip.split()
+                    url = line_split[2]
+                    version = line_split[1]
+        elif prod == "FileZilla" or prod == "FileZilla_release":
+            for item in manifest_str.split("\n"):
+                if "release" in item:
+                    item_strip = item.strip()
+                    line_split = item_strip.split()
+                    url = line_split[2]
+                    version = line_split[1]
+
+        if not version or not url:
+            raise ProcessorError("Version or URL not found")
+
+        self.env["version"] = version
+        self.env["url"] = url
+        self.output("Found URL %s" % self.env["url"])
 
 if __name__ == "__main__":
     processor = FileZillaURLProvider()
