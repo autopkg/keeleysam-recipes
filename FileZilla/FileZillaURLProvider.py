20ce027343ffd3a9f0a3ab591b1e0850b980fe76
Elliot Jordan <elliot@elliotjordan.com>
2019-08-31 13:51:04 -0700

    Apply suggestions from python-modernize

--- FileZilla/FileZillaURLProvider.py
+++ FileZilla/FileZillaURLProvider.py
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import
 import urllib2
 import re
 import ssl
--- GoogleTalkPlugin/MunkiPkginfoReceiptsEditor.py
+++ GoogleTalkPlugin/MunkiPkginfoReceiptsEditor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from __future__ import absolute_import
 from FoundationPlist import readPlist, writePlist
 from autopkglib import Processor, ProcessorError
 
